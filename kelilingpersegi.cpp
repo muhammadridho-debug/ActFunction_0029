cf6af081d6d8c60badc7be600058750f6dbef333
muhammadridho-debug <idomuhammad11@gmail.com>
2026-03-02 12:34:53 +0700

    menambahkan library, deklarasi, prosedur, dan menambahkan komputasi

---
+++ kelilingpersegi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int panjang, lebar;
+
+void input()
+{
+    cout << "Masukkan Panjang : ";
+    cin >> panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> lebar;
+}
+
+int kelilingPersegi(int a, int b)
+{
+    return 2 * (a + b);
+}
